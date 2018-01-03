90dca4aa1c956d78cb0032dd10bf09d3189511b5
wayanpermana <aco.wayan@outlook.com>
2018-01-04 02:43:08 +0800

    todo : add plus survei

--- views/mitra/_form.php
+++ views/mitra/_form.php
@@ -122,13 +122,35 @@ $this->registerJs($js);
     )?>
 
     <?= $form->field($model, 'pengalaman_survei')->textarea(['rows' => 6]) ?>
-    <?= Html::dropDownList('s_id', null,
+    <!-- <?= Html::dropDownList('s_id', null,
         ArrayHelper::map(Survei::find()->all(), 'id', 'nama'),
         [
             'prompt'=>'Pilih Survei',
             'class' => 'form-control',
         ]
-      ) ?>
+      ) ?> -->
+
+    <div class="form-group field-mitra-pengalaman_survei required">
+        <label class="control-label" for="mitra-pengalaman_survei">Pengalaman Survei</label>
+        <!-- <textarea id="mitra-pengalaman_survei" class="form-control" name="Mitra[pengalaman_survei]" rows="6" aria-required="true">gdgdg</textarea> -->
+        <div id="list-survei">
+            <div class="input-group">
+                <select class="form-control" id="educationDate" name="educationDate[]">
+                    <option value="">Date</option>
+                    <option value="2015">2015</option>
+                    <option value="2016">2016</option>
+                    <option value="2017">2017</option>
+                    <option value="2018">2018</option>
+                </select>
+                <div class="input-group-btn">
+                    <button class="btn btn-success" type="button" onclick="education_fields();"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="help-block"></div>
+    </div>
+
 
     <!-- <?= $form->field($model, 'penguasaan_kendaraan_motor')->textInput() ?> -->
     <div class="form-group field-mitra-penguasaan_kendaraan_motor required">
@@ -231,6 +253,57 @@ $this->registerJs($js);
 
 </div>
 
+<!-- bootsnip -->
+<div class="panel panel-default">
+  <div class="panel-heading">Dynamic Form Fields - Add & Remove Multiple fields</div>
+  <div class="panel-heading">Education Experience</div>
+  <div class="panel-body">
+  
+  <div id="education_fields">
+          
+        </div>
+       <div class="col-sm-3 nopadding">
+  <div class="form-group">
+    <input type="text" class="form-control" id="Schoolname" name="Schoolname[]" value="" placeholder="School name">
+  </div>
+</div>
+<div class="col-sm-3 nopadding">
+  <div class="form-group">
+    <input type="text" class="form-control" id="Major" name="Major[]" value="" placeholder="Major">
+  </div>
+</div>
+<div class="col-sm-3 nopadding">
+  <div class="form-group">
+    <input type="text" class="form-control" id="Degree" name="Degree[]" value="" placeholder="Degree">
+  </div>
+</div>
+
+<div class="col-sm-3 nopadding">
+  <div class="form-group">
+    <div class="input-group">
+      <select class="form-control" id="educationDate" name="educationDate[]">
+      
+        <option value="">Date</option>
+        <option value="2015">2015</option>
+        <option value="2016">2016</option>
+        <option value="2017">2017</option>
+        <option value="2018">2018</option>
+      </select>
+      <div class="input-group-btn">
+        <button class="btn btn-success" type="button"  onclick="education_fields();"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> </button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="clear"></div>
+  
+  </div>
+  <div class="panel-footer"><small>Press <span class="glyphicon glyphicon-plus gs"></span> to add another form field :)</small>, <small>Press <span class="glyphicon glyphicon-minus gs"></span> to remove form field :)</small></div>
+  <div class="panel-footer"><small><em><a href="http://shafi.info/">More Info - Developer Shafi (Bangladesh)</a></em></em></small></div>
+</div>
+
+
+
 <script>
     $(function(){
         $('.spinner .btn:first-of-type').on('click', function() {
@@ -310,6 +383,24 @@ $this->registerJs($js);
     
     
     });
+
+    // bootsnip
+    var room = 1;
+    function education_fields() {
+    
+        room++;
+        // var objTo = document.getElementById('education_fields')
+        var objTo = document.getElementById('list-survei')
+        var divtest = document.createElement("div");
+        divtest.setAttribute("class", "form-group removeclass"+room);
+        var rdiv = 'removeclass'+room;
+        divtest.innerHTML = '<div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="Schoolname" name="Schoolname[]" value="" placeholder="School name"></div></div><div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="Major" name="Major[]" value="" placeholder="Major"></div></div><div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="Degree" name="Degree[]" value="" placeholder="Degree"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><div class="input-group"> <select class="form-control" id="educationDate" name="educationDate[]"><option value="">Date</option><option value="2015">2015</option><option value="2016">2016</option><option value="2017">2017</option><option value="2018">2018</option> </select><div class="input-group-btn"> <button class="btn btn-danger" type="button" onclick="remove_education_fields('+ room +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button></div></div></div></div><div class="clear"></div>';
+        
+        objTo.appendChild(divtest)
+    }
+    function remove_education_fields(rid) {
+        $('.removeclass'+rid).remove();
+    }
 
 
 </script>
