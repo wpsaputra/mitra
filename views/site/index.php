c08a6be4bb6a6acbf9ee81832864d53484d3c470
wayanpermana <aco.wayan@outlook.com>
2018-01-05 01:03:00 +0800

    todo add chart

--- views/site/index.php
+++ views/site/index.php
@@ -1,20 +1,37 @@
 <?php
 
 /* @var $this yii\web\View */
+use miloschuman\highcharts\HighchartsAsset;
+use miloschuman\highcharts\Highcharts;
 
+HighchartsAsset::register($this)->withScripts(['highstock', 'modules/exporting', 'modules/drilldown']);
 $this->title = 'My Yii Application';
+echo Highcharts::widget([
+    'options' => [
+        'chart' => ['type' => 'bar'],
+        'title' => ['text' => 'Fruit Consumption'],
+        'xAxis' => [
+            'categories' => ['Apples', 'Bananas', 'Oranges']
+        ],
+        'yAxis' => [
+            'title' => ['text' => 'Fruit eaten']
+        ],
+        'series' => [
+            ['name' => 'Jane', 'data' => [1, 0, 4]],
+            ['name' => 'John', 'data' => [5, 7, 3]]
+        ]
+    ]
+ ]);
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
     <div class="body-content">
+        <div class="row">
+            <div id="tespie" class="col-md-12">
+            
+            
+            </div>
+        
+        </div>
 
         <div class="row">
             <div class="col-lg-4">
@@ -51,3 +68,70 @@ $this->title = 'My Yii Application';
 
     </div>
 </div>
+
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script>
+    Highcharts.chart('tespie', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Historic World Population by Region'
+        },
+        subtitle: {
+            text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+        },
+        xAxis: {
+            categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Population (millions)',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' millions'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 80,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Year 1800',
+            data: [107, 31, 635, 203, 2]
+        }, {
+            name: 'Year 1900',
+            data: [133, 156, 947, 408, 6]
+        }, {
+            name: 'Year 2012',
+            data: [1052, 954, 4250, 740, 38]
+        }]
+    });
+
+</script>
