24b188b41ff834d5520cac3c2c65091c8d5f1258
wpsaputra <wayanpermana93@gmail.com>
2018-01-10 16:00:25 +0800

    Fix export, todo excel template

--- models/UploadForm.php
+++ models/UploadForm.php
@@ -44,12 +44,40 @@ class UploadForm extends Model
                 $writer->setUseBOM(true);
                 $writer->save($path.".csv");
 
-                \Yii::$app->db->createCommand('LOAD DATA INFILE :path
-					INTO TABLE `mitra`
-					FIELDS TERMINATED BY \',\'
-					ENCLOSED BY \'"\'
-					LINES TERMINATED BY \'\n\'
-                    IGNORE 1 LINES;')->bindValues([':path'=>\Yii::$app->basePath."/web/uploads/".$fileName.".csv"])->execute();
+                // \Yii::$app->db->createCommand('LOAD DATA INFILE :path
+				// 	INTO TABLE `mitra`
+				// 	FIELDS TERMINATED BY \',\'
+				// 	ENCLOSED BY \'"\'
+				// 	LINES TERMINATED BY \'\n\'
+                //     IGNORE 1 LINES;')->bindValues([':path'=>\Yii::$app->basePath."/web/uploads/".$fileName.".csv"])->execute();
+
+                ini_set('max_execution_time', 300);
+                $row = 1;
+                $dokumen = 'mitra';
+				if (($handle = fopen(\Yii::$app->basePath."/web/uploads/".$fileName.".csv", "r")) !== FALSE) {
+					while (($data = fgetcsv($handle, 2000, ",")) !== FALSE) {
+                        $num = count($data);
+                        
+                        $row++;
+                        if($row==2){
+                            continue;
+                        }
+
+                        $arr_value = array();
+                        $cmd = "REPLACE INTO ".$dokumen." VALUES (";
+                        for ($c=0; $c < $num; $c++) {
+                            if($c==$num-1){
+                                $cmd = $cmd.":data".$c;
+                            }else{
+                                $cmd = $cmd.":data".$c.", ";
+                            }
+                            $arr_value[":data".$c] = $data[$c];
+                        }
+                        $cmd = $cmd.")";
+                        \Yii::$app->db->createCommand($cmd)->bindValues($arr_value)->execute();
+					}
+					fclose($handle);
+				}
                 
                 unlink($path);
                 unlink($path.".csv");
